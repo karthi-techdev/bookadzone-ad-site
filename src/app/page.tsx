71e490ff1f177527d045050abd140cb3620882a7
karthi <kr748283@gmail.com>
2025-11-03 18:46:47 +0530

    old-code-revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -866,11 +866,6 @@ export default function Home() {
     </div>
   </section>
 
-    {/* Parallax Background Elements */}
-    <div className="parallax-bg fixed top-0 left-0 w-full h-full -z-10 pointer-events-none">
-      <div className="absolute top-1/4 left-1/4 w-72 h-72 bg-purple-500/10 rounded-full blur-3xl"></div>
-      <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl"></div>
-    </div>
     </main>
   );
 }
